b52c7ed839066d9a9508fe422d32411036ff3907
Md. Hasanur Rahman <hasanurhasu127@gmail.com>
2023-08-20 20:43:21 +0600

    added entry element

--- js/function.js
+++ js/function.js
@@ -1,6 +1,16 @@
 let total = 0;
 
 function getPrice(data){
+    
+    const itemEntry = document.getElementById('calculation-entry');
+    const itemTittleName = data.childNodes[3].childNodes[3].innerText;
+    const count = itemEntry.childElementCount;
+    const p = document.createElement('p');
+    p.innerText = `${count+1}. ${itemTittleName}`;
+    p.classList.add('text-2xl', 'font-medium')
+    itemEntry.appendChild(p);
+    console.log(itemTittleName);
+
     const itemPriceString = data.childNodes[3].childNodes[5].childNodes[1].innerText;
     const itemPrice = parseFloat(itemPriceString);
     total = total + itemPrice;
