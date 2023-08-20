8c341a9ff325da1e748f2bd5223bf59c5bb805e8
Md. Hasanur Rahman <hasanurhasu127@gmail.com>
2023-08-20 21:11:03 +0600

    added coupon button and purchase button disabled and enabled

--- js/function.js
+++ js/function.js
@@ -1,12 +1,12 @@
 let total = 0;
 
-function getPrice(data){
-    
+function getPrice(data) {
+
     const itemEntry = document.getElementById('calculation-entry');
     const itemTittleName = data.childNodes[3].childNodes[3].innerText;
     const count = itemEntry.childElementCount;
     const p = document.createElement('p');
-    p.innerText = `${count+1}. ${itemTittleName}`;
+    p.innerText = `${count + 1}. ${itemTittleName}`;
     p.classList.add('text-2xl', 'font-medium')
     itemEntry.appendChild(p);
     console.log(itemTittleName);
@@ -17,11 +17,27 @@ function getPrice(data){
     console.log(total);
 
     setValueTotal('total-price', total);
+
+    if (total >= 200) {
+        const couponBtn = document.getElementById('btn-coupon');
+        couponBtn.classList.remove('bg-gray-500');
+        couponBtn.classList.add('bg-[#E527B2]');
+        couponBtn.removeAttribute('disabled')
+    }
+
+    if (total >= 0) {
+        const purchaseBtn = document.getElementById('purchase-btn');
+        purchaseBtn.classList.remove('bg-gray-500');
+        purchaseBtn.classList.add('bg-[#E527B2]');
+        purchaseBtn.removeAttribute('disabled')
+    }
+
+
 }
 
 
 function setValueTotal(elementID, value) {
     const totalValueElement = document.getElementById(elementID);
-    totalValueElement.innerText = value;
+    totalValueElement.innerText = value.toFixed(2);
 }
 
